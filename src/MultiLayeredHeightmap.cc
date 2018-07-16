55d3d24a0804668aae9d69ec9929d3fd5c1c3f3f
Edi Cikovic <edi.cikovic@gmail.com>
2018-07-16 22:07:31 +0200

    Use mDisplacement for y coordinate 2

--- src/MultiLayeredHeightmap.cc
+++ src/MultiLayeredHeightmap.cc
@@ -978,7 +978,7 @@ void MultiLayeredHeightmap::LoadSplatmap(){
     //    }
 
         //beach
-        if (mPositions.at(i).y < 14 && fScale_slope <= fRange4_slope){
+        if (mDisplacement.at(i) < 14 && fScale_slope <= fRange4_slope){
             a = 1.f;
             r = 0.f;
             g = 0.f;
